1d78c71401a206b678cc0da458b14da1219f2b7b
uhrybok <u.hrybok@gmail.com>
2025-09-11 15:23:46 +0300

    gh 2

--- sockets/client.py
+++ sockets/client.py
@@ -5,17 +5,20 @@ HOST = ('127.0.0.1', 7007)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-sock.connect(HOST)
+try:
+    sock.connect(HOST)
 
-print("--start clirnt--")
+    print("--start client--")
 
-while 1:
-    text = input("Enter message (or 'bye' or 'stop'): ")
-    sock.sendall(text.encode("utf-8"))
+    while 1:
+        text = input("Enter message (or 'bye' or 'stop'): ")
+        sock.sendall(text.encode("utf-8"))
     
-    if text.lower() == 'bye' or text.lower() == 'stop':
-        break
+        if text.lower() == 'bye' or text.lower() == 'stop':
+            break
 
-sock.close()
+    sock.close()
+except:
+    print("connection error")    
 
 print("--stop client--")
